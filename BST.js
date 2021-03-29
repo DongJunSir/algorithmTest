70f3956b767c2e3a354dd590f437fe03eab85af8
DongJunSir <e_dongjun55@163.com>
2021-03-29 23:43:33 +0800

    二叉搜索树

---
+++ BST.js
@@ -0,0 +1,99 @@
+class Node {
+    constructor(element, parent) {
+        this.element = element;
+        this.parent = parent;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+        this.size = 0;
+    }
+    add(element) {
+        // 开始创建树的结构
+        if (this.root === null) {
+            // 创建根节点
+            this.root = new Node(element, null)
+            this.size++
+        } else {
+            // 计算根节点和当前添加节点的差值
+            let parentNode = this.root
+            let currentNode = this.root;
+            let compare;
+            while (currentNode !== null) {
+                parentNode = currentNode
+                compare = element - currentNode.element
+                if (compare < 0) {
+                    currentNode = currentNode.left
+                } else {
+                    currentNode = currentNode.right
+                } 
+            }
+            if (compare < 0) {
+                // 如果小于0，就添加到左子树
+                let node = new Node(element, parentNode)
+                parentNode.left = node
+            } else {
+                // 如果大于0，就添加到右子树
+                let node = new Node(element, parentNode)
+                parentNode.right = node
+            }
+            this.size++
+        }
+    }
+    // 前序遍历
+    preorderTraversal() {
+        const traversal = (node) => {
+            if (node == null) return;
+            console.log(node.element)
+            traversal(node.left)
+            traversal(node.right)
+        }
+        traversal(this.root)
+    }
+    // 中序遍历
+    inorderTraversal() {
+        const traversal = (node) => {
+            if (node == null) return;
+            traversal(node.left)
+            console.log(node.element)
+            traversal(node.right)
+        }
+        traversal(this.root)
+    }
+    // 后续遍历
+    postorderTraversal() {
+        const traversal = (node) => {
+            if (node == null) return;
+            traversal(node.left)
+            traversal(node.right)
+            console.log(node.element)
+        }
+        traversal(this.root)
+    }
+    levelOrderTraversal() {
+        if (this.root == null) return;
+        // 层序遍历 对应每一层都按顺序遍历一遍出来
+        let stack = [this.root]
+        let index = 0
+        while (stack[index]) {
+            if (stack[index].left) {
+                stack.push(stack[index].left)
+            }
+            if (stack[index].right) {
+                stack.push(stack[index].right)
+            }
+            index++
+        }
+    }
+}
+
+let bst = new BST()
+let arr = [10, 8, 6, 19, 15, 22, 20]
+arr.forEach(item => {
+    bst.add(item)
+})
+bst.levelOrderTraversal()
--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-## algorithmTest
-=======
 # algorithmTest
->>>>>>> a8f59571f4c86afc8cb6c52d70b59831729ffa93
